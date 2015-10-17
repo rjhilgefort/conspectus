18a654d958a148a6becb6573e3a3fc1af16b3c41
Rob Hilgefort <rjhilgefort@gmail.com>
2015-10-17 11:37:21 -0400

    Add `default` default to filter

--- _plugins/default.rb
+++ _plugins/default.rb
@@ -1,8 +1,10 @@
-# https://github.com/Shopify/liquid/pull/267
+# @author rjhilgefort
+# inspiration came from:
+#     https://github.com/Shopify/liquid/pull/267
 
 module Jekyll
     module DefaultFilter
-        def default(input, default_value = "")
+        def default(input, default_value = "[unspecified]")
             is_blank = input.respond_to?(:empty?) ? input.empty? : !input
             is_blank ? default_value : input
         end
